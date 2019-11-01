2e7147a0478ee363135a63117121f0898cad520f
Karl Atkinson <karl.atkinson@gmail.com>
2019-11-01 10:45:14 +0000

    updated bucket name

--- deploy/state.tf
+++ deploy/state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "karl-terraform-state"
+    bucket  = "karls-terraform-state-bucket"
     key     = "terraform-workspace-demo/state.tfstate"
     region  = "eu-west-1"
     encrypt = true
